bcfb6aebcf3860476066229d3372ee87e84e4e15
zhen <xzz2013@gmail.com>
2022-03-11 17:11:44 +0800

    data parse in page1

--- src/app/adaptive/adaptive.component.ts
+++ src/app/adaptive/adaptive.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as d3 from "d3"
+import {select} from "d3";
 @Component({
   selector: 'app-adaptive',
   templateUrl: './adaptive.component.html',
@@ -8,88 +9,118 @@ import * as d3 from "d3"
 })
 export class AdaptiveComponent implements OnInit {
   drawCost():void{
-    let nodes = [[1,2,3],[2,3],[4,5],[],[]];
-    let links = [{source:0,target:1},{source:0,target:2},{source:1,target:3},{source:1,target:4}]
-    let gravity = [0,1000,100,200,200]
-    const width=1200,height=600;
+    const data = {content:[1,2,3],children:[{content:[1,2],children:[{content:[3]}]},{content:[3]}]};
+    const root = d3.hierarchy(data);
+    const treelayout = d3.tree().size([600,400])
+    treelayout(root)
 
-    const svg = d3.select("#costGraph")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [-width / 2, -height / 2, width, height])
-      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+    const update = ()=>{
+      treelayout(root);
+      const lines = g.select(".gLink")
+        .selectAll("line")
+        .data(root.links())
+        .join("line")
+        //@ts-ignore
+        .attr('x1', d=>d.source.x+100).attr('y1', d=>d.source.y+80).attr('x2', d=>d.target.x+100).attr('y2', d=>d.target.y);
 
-    const forceNode = d3.forceManyBody().strength(-1000);
-    const forceLink = d3.forceLink(links);
-    const forceCollision = d3.forceCollide().radius(30);
-    const forceX = d3.forceX().x((_,i)=>gravity[i]);
-    // @ts-ignore
-    const simulation = d3.forceSimulation(nodes)
-      .force("link",forceLink)
-      .force("charge",forceNode)
-      .force("center",d3.forceCenter())
-      .force("collision",forceCollision)
-      .force("x",forceX)
-      .on("tick",ticked)
+      const nodes=g.selectAll(".rect")
+        .data(root.descendants())
+        .join("g")
+        //@ts-ignore
+        .attr("transform",d=>`translate(${d.x},${d.y})`)
+        .classed("rect",true)
+        .on("click",(e,d) => {
+          console.log(d)
+          if (d.children) {//
+            //@ts-ignore
+            d._children = d.children;
+            //@ts-ignore
+            d.children = null;
+          } else {          //@ts-ignore
+            d.children = d._children;          //@ts-ignore
+            d._children = null;
+          }
+          update();
+        })
 
-    const link = svg.append("g")
-      .attr("stroke","#999")
+      nodes.append("rect")
+        .attr("stroke","#87CEEB")
+        .attr("stroke-width",2)
+        .attr("stroke-dasharray",5)
+        .attr("fill","#fff")
+        .attr("fill-opacity",0)
+        .attr('width',200)
+        .attr('height',80)
+        .attr('rx',10)
+        .attr('ry',10);
+
+      nodes.append("g")
+        .attr("stroke","#000")
+        .attr("stroke-width",2)
+        .attr("fill","#fff")
+        .selectAll("rect")
+        .data(d=>d.data.content)
+        .join("rect")
+        .attr("height",30)
+        .attr("width",30)
+        .attr('x',(_,i)=>i*30+10)
+        .attr('y',40)
+
+      nodes.append("g")
+        .selectAll("text")
+        .data(d=>d.data.content)
+        .join("text")
+        .attr('x',(_,i)=>i*30+18)
+        .attr('y',65)
+        .html(d=>d.toString());
+
+      nodes.append("ellipse")
+        .attr('cx',170)
+        .attr('cy',55)
+        .attr('rx',25)
+        .attr('ry',20)
+        .attr('fill','#999')
+        .attr('stroke','#000');
+    }
+
+    const svg = d3.select("#costGraph").append("svg")
+      .attr("width", 1600)
+      .attr("height", 800)
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 25);
+
+    const defs = svg.append("defs");
+
+    const arrowMarker = defs.append("marker")
+      .attr("id","arrow")
+      .attr("markerUnits","strokeWidth")
+      .attr("markerWidth",12)
+      .attr("markerHeight",12)
+      .attr("viewBox","0 0 12 12")
+      .attr("refX",6)
+      .attr("refY",6)
+      .attr("orient","auto");
+
+    const arrow_path = "M2,2 L10,6 L2,10 L6,6 L2,2";
+
+    arrowMarker.append("path")
+      .attr("d",arrow_path)
+      .attr("fill","#000");
+
+    const g = svg.append('g').attr("transform","translate(0,5)");
+
+    const lines = g.append("g")
+      .classed("gLink",true)
+      .attr("fill", "none")
+      .attr("stroke", "#000")
       .attr("stroke-opacity", 0.9)
-      .attr("stroke-width", 2)
-      .attr("stroke-linecap", "round")
-      .selectAll("line")
-      .data(links)
-      .join("line");
+      .attr("stroke-width", 1.5)
+      .attr("marker-end","url(#arrow)");
 
-    const node = svg.append("g")
-      .attr("fill","#FFF")
-      .attr("fill-opacity",0)
-      .attr("stroke","#87ceeb")
-      .attr("stroke-dasharray",10)
-      .selectAll("rect")
-      .data(nodes)
-      .join("rect")
-      .attr("width",200)
-      .attr("height",50)
-      .attr("rx",10)
-      .attr("ry",10)
-      // @ts-ignore
-      .call(drag(simulation));
-    // @ts-ignore
-    node.append('text').attr("dx", 12)
-      .attr("dy", ".35em").text("123");
-    function ticked() {
-      // @ts-ignore
-      link.attr("x1", d => d.source.x+100).attr("y1", d => d.source.y+50).attr("x2", d => d.target.x+100).attr("y2", d => d.target.y);
+    update();
 
-      // @ts-ignore
-      node.attr("x", d => d.x).attr("y", d => d.y);
-    }
 
-    function drag(simulation: any) {
-      function dragstarted(event: any) {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
-        event.subject.fx = event.subject.x;
-        event.subject.fy = event.subject.y;
-      }
-
-      function dragged(event: any) {
-        event.subject.fx = event.x;
-        event.subject.fy = event.y;
-      }
-
-      function dragended(event: any) {
-        if (!event.active) simulation.alphaTarget(0);
-        event.subject.fx = null;
-        event.subject.fy = null;
-      }
-
-      return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-    }
   }
 
 
